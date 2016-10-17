bf635ad0e2e5d5457faf57332e6e80fbecd4aba7
rafabripi <rafaelbripi@gmail.com>
2016-10-16 23:18:41 -0500

    add validator

---
+++ FieldGroup.js
@@ -0,0 +1,25 @@
+/**
+* @Author: Rafael Bribiesca
+* @Date:   2016-08-14T22:39:53-05:00
+* @Email:  rafaelbripi@gmail.com
+* @Project: myIdentity
+* @Last modified by:   Rafael Bribiesca
+* @Last modified time: 2016-10-16T23:01:56-05:00
+*/
+
+
+
+import React from "react";
+import {FormGroup,ControlLabel,FormControl,HelpBlock} from "react-bootstrap";
+
+const FieldGroup = ({ id, label, help, ...props }) => {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
+
+export default FieldGroup;
